45b08c69081a8ec14139188f72b0b9b1cc469a2c
Sergey Manzhak <manzhak.ru@gmail.com>
2018-03-13 00:37:17 +0300

    Add form for create data

--- inc/create.php
+++ inc/create.php
@@ -5,3 +5,13 @@
  * Date: 12.03.2018
  * Time: 21:24
  */
+
+?>
+<form action="index.php?action=c" method="post">
+    <input type="text" name="name">
+    <input type="text" name="author">
+    <input type="text" name="year">
+    <input type="text" name="isbn">
+    <input type="text" name="genre">
+    <input type="submit" name="add" value="Обновить">
+</form>
